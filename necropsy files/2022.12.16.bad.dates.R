49e27b169a0637a66b31a3b14e107b97dc68329b
otrimskig <gotrimski@gmail.com>
2022-12-27 20:06:14 -0700

    x

--- necropsy files/2022.12.16.bad.dates.R
+++ necropsy files/2022.12.16.bad.dates.R
@@ -8,3 +8,10 @@ library(lubridate)
 readRDS("bad_dates.rds")->bad_dates
 
 
+view(bad_dates)
+
+#first approach. Check genotypes. If not in experimental group,
+#much easier to exclude. 
+
+readRDS("colony_genos2.rds")->colony_genos
+
