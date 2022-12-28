079bdb4c25d10a010e12bc3b8b01d459dfe3aa2b
otrimskig <gotrimski@gmail.com>
2022-12-28 11:29:24 -0700

    x

--- cohorts2/2022.12.27-cohorts2.R
+++ cohorts2/2022.12.27-cohorts2.R
@@ -1,5 +1,8 @@
 ################################################################
 #2022.12.27
+#2022.12.28
+#adding dates and corrected genotypes to colony info. 
+
 
 library(tidyverse)
 library(lubridate)
@@ -66,6 +69,122 @@ rename(g_dob = dob.x)%>%
   
   
   
+#adding in dob from bad_dates. 
+cohorts1.2%>%
+  filter(is.na(dob))%>%
+  select(-dob)%>%
+  left_join(bad_dates%>%select(mouse_num, dob), by = "mouse_num")%>%
+  full_join(cohorts1.2%>%filter(!is.na(dob)))->cohorts1.3
+  
+  
 
 
+cohorts1.3%>%
+  
+  #removing unnecessary column. 
+  select(-c_dob)%>%
+  
+  relocate(dob, .after = "sex")%>%
+  relocate(death_date, .after = "mri_imaging_date")%>%
+  
+  #adding exclusion data and meta notes. 
+  mutate(exclude = if_else(is.na(death_date), "2", exclude))%>%
+  mutate(metadata = if_else(is.na(death_date), "contested death date", metadata))%>%
+  
+  #based on comments, these mice were both sacked for end date - even though their 
+  #death dates are disputed. 
+  mutate(exclude = if_else(mouse_num == "29389"| mouse_num=="29390", 
+                           NA_character_, exclude))%>%
+  
+  mutate(metadata = if_else(mouse_num == "29389"| mouse_num=="29390", 
+                           "contested death dates but notes say sacked for endpoint", metadata)
+         )->cohorts1.4
+  
+#not sure why some date columns aren't in date form. Fixing that.  
+cohorts1.4%>%
+  mutate_at(c("injection_date", "tumor_noticed", "behavior_noticed",
+              "mri_imaging_date", "end_date"), mdy
+            )->cohorts1.5
+  
+ 
+#resolving additional dates in separate df.
+cohorts1.5%>%
+  select(mouse_num, c(dob:end_date), comments)%>%
+  
+  filter(is.na(death_date))%>%
+  select(where(function(x) any(!is.na(x))))%>%
+  left_join(bad_dates%>%
+              select(mouse_num, g_death_date, c_death_date),by = "mouse_num")%>%
+  mutate(dd = NA)%>%
+  
+  #dates resolved with notes attached in metadata. 
+  mutate(dd = if_else(mouse_num == "23297"|
+                        mouse_num =="25034", c_death_date, as.Date(dd)))%>%
+  mutate(metadata = if_else(mouse_num == "23297"|
+                              mouse_num =="25034", "behavior data was after purported death date in genotyping. Used cohort dd.", 
+                            NA_character_))%>%
+ 
+  #dates resolved with notes attached in metadata. 
+  mutate(metadata = if_else(is.na(dd)&!is.na(end_date), 
+                            "end date in cohort data was same as cohort death date, so used cohort death date.",
+                            metadata))%>%
+  
+  mutate(dd = if_else(is.na(dd)&!is.na(end_date), end_date, as.Date(dd)))%>%
+
+  select(-dob, -injection_date)%>%
+  
+  filter(!is.na(dd))%>%
+  select(where(function(x) any(!is.na(x))))%>%
+  select(mouse_num, dd, metadata)->resolved_dd1
+
+
+#now adding all other information to the resolved dates. 
+cohorts1.5%>%
+  semi_join(resolved_dd1, by = "mouse_num")%>%
+  left_join(resolved_dd1, by = "mouse_num")%>%
+  
+  mutate(exclude = NA)%>%
+  select(-death_date)%>%
+  rename(death_date = dd)%>%
+  
+  unite("metadata", c("metadata.x", "metadata.y"), sep = " .. ")->resolved_dd2
+
+#removing resolved date mouse numbers and replacing with updated obs
+#into main dataset. 
+cohorts1.5%>%
+  anti_join(resolved_dd2, by = "mouse_num")%>%
+  
+  #now has all notes of changes made in metadata.
+  #those with still-unresolved death dates are left NA, plus
+  #noted with a "2" in the exclude column,
+  #plus noted the issue in metadata. 
+  full_join(resolved_dd2)->cohorts1.6
+
+
+cohorts1.6%>%
+  filter(is.na(death_date))%>%
+  
+  left_join(bad_dates)%>%
+  
+  mutate(death_date = as.Date(c_death_date))%>%
+  select(-c(index:last_col()))%>%
+  
+  #using rick's cohort data for last remaining unresolved dates.
+  #retaining 2 in exclude column until more satisfactory resolution. 
+  
+  mutate(metadata = paste0(metadata, " .. ", 
+      "used rick's cohort data as correct death date. No other justification other than hoping this was most accurate. 
+For the corrected data in this group, all cohort dds were AFTER geno dds. Retaining '2' in exclusion column until more satisfactory resolution.")
+)->resolved_dd3
+  
+
+
+cohorts1.6%>%
+  #adding resolved data to main df. 
+  anti_join(resolved_dd3, by = "mouse_num")%>%
+  full_join(resolved_dd3)->cohorts2
+
+
+#all date obs are now fully resolved or resolved with notes in exclusion/metadata columns.  
+saveRDS(cohorts2, "cohorts2.rds")
 
