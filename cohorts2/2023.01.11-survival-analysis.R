69e954266bfec33f532d2308e80f8102308dd879
Garrett Otrimski <u1413890@hci.utah.edu>
2023-01-12 11:51:28 -0700

    x

--- cohorts2/2023.01.11-survival-analysis.R
+++ cohorts2/2023.01.11-survival-analysis.R
@@ -10,136 +10,172 @@ library(survival)
 
 
 #reading in previously set color data. 
-read_csv("../mouse cohorts/cohort_legend.csv")->cohort_legend
+read_csv("../mouse cohorts/cohort_legend.csv")->cohort_legend1
 
 #reading in current dataset. 
 readRDS("cohort3_survival.rds")->cohort_survival
+
+
+
+
 
 
 ######################################################
 #color key
 #updating color key to match to "genes_ko" to "cohort" notation. 
 ######################################################
-#getting list of cohorts in dataset, with n for each. 
+
+#getting list of cohorts from main dataset, with n's for each. 
 cohort_survival%>%
   count(cohort)%>%
+  
   #removing NA cohorts 
-  filter(!is.na(cohort))->cohort_stats
+  filter(!is.na(cohort)
+         
+         #saving cohort names and n's. 
+          )->cohort_stats
+
+
 
 #getting match key for cohort <-> gene_ko. 
+#need this to match "genes_ko" to "cohorts". Color key data is linked to genes_ko. 
+#groups are also ORDERED alphabetically by genes_ko, which will be important later for graph. 
 read_csv("ref/cohort_key.csv")->join_key
 
-#adding cohort names to legend key. 
-cohort_stats%>%
-  
 
-  
+#joining cohort names with previously created color scheme using join_key. 
+cohort_stats%>%
   left_join(join_key)%>%
+  left_join(cohort_legend1, by = "genes_ko")%>%
   
-  
-  left_join(cohort_legend, by = "genes_ko")%>%
-  
-  
-#creating cohort_info rds file, to which I can later add summary data,
+#creating cohort_legend rds file, to which I can later add summary data,
 #and use for color-matching consistency.
-  saveRDS("cohort_info.rds")
+#dataset contains: genes_ko, cohorts, n's, color, hex for that color.   
+  saveRDS("cohort_legend.rds")
 
 
 #cleaning up
-rm("cohort_legend", "cohort_stats", "join_key")
+#removing unnecessary objects from environment that won't be needed any longer. 
+rm("cohort_legend1", "cohort_stats", "join_key")
+
+
+
+
+
+
+
 
 ##################################################################
+
+
+
+#reading in necessary data. #legend
+readRDS("cohort_legend.rds")->cohort_legend
+
+#read in already above, just a reminder:
+#main dataset:
+#       cohort_survival
+
+#previously plotted data:
+read_csv("../mouse cohorts/survival_compiled.csv")->cohort_prev
+
+
+#checking for mismatches in current vs. old data. 
+#since there were many corrections, I know there will be many mismatches.
+#just checking for any obvious ones. 
+# 
+cohort_prev%>%
+  #selecting and quick clean-up of imported data
+  select(mouse_num, age_death_capped)%>%
+  mutate(mouse_num = as.character(mouse_num))%>%
+
+  #joining dfs and removing excluded data.
+  full_join(cohort_survival, by = "mouse_num")%>%
+  filter(exclude != "1"| is.na(exclude))%>%
+
+  #calculating age of death as "aod"
+  mutate(aod = death_date-dob)%>%
+  mutate(aod2 = as.numeric(aod))%>%
+
+  #selecting only necessary columns...
+  select(mouse_num, aod2, age_death_capped)%>%
+
+  #filtering for where they're not equal.
+  filter(aod2 != age_death_capped)%>%
+
+  view()
+
+#overall not super-informative...
+#there are many differences because there were many corrections to original df. 
+#however, there are mice #s included in the updated data that were not present in original df. 
+#so there will still be many mice that will need to be age-corrected
+#which were sacked at d147-148 accidentally that should have been end-date. 
+
+rm(cohort_prev)
+
 
 #continuing with analysis. 
 
 
-readRDS("cohort_info.rds")->cohort_info
-#read in already above, just a reminder:
-#readRDS("cohort3_survival.rds")->cohort_survival
-read_csv("../mouse cohorts/survival_compiled.csv")->cohort_prev
-
-
-##prior to actually plotting, need to ensure data matches previously corrected data-especially for age of mice. 
-cohort_prev%>%
-  select(mouse_num, age_death_capped)%>%
-  mutate(mouse_num = as.character(mouse_num))%>%
-  full_join(cohort_survival, by = "mouse_num")%>%
-  filter(exclude != "1"| is.na(exclude))%>%
-  
-  mutate(aod = death_date-dob)%>%
-  mutate(aod2 = as.numeric(aod))%>%
-  select(mouse_num, aod2, age_death_capped)%>%
-  
-  filter(aod2 != age_death_capped)%>%
-  
-  view()
-#overall not super-informative...continuing with analysis. 
-
-
-
-
+#############################################################################
 #############################################################################
 
-#taking dataset into form friendly for survival curve. 
+
+
+
+#taking main dataset into form friendly for survival curve. 
+
+
 cohort_survival%>%
   #getting age_death
   mutate(age_death = as.numeric(death_date-injection_date))%>%
+  
   #removing excluded mice from data. 
   filter(exclude != "1" | is.na(exclude))%>%
-  
-  mutate(event = case_when(age_death >149~0,
-                           age_death <=149~1))%>%
-  
-  #removing all mice without a cohort. 
+  #and mice without a cohort. 
   filter(!is.na(cohort))%>%
-  left_join(cohort_info%>%select(genes_ko, cohort), by = "cohort")%>%
-  arrange(genes_ko)->cohort_survival2
+  
+  
 
-
-
-
-cohort_survival2%>%
+  #adding in genes_ko info and connecting to cohort. 
+  left_join(cohort_legend%>%select(genes_ko, cohort), by = "cohort")%>%
+  
+  #selecting info necessary for plotting. 
   select(cohort, mouse_num, age_death, genes_ko)%>%
+  
+  
+  #some very basic data edits to account for mice sacked on the wrong day.
+  #will go through in more detail to see if can have a better idea of which 
+  #this actually happened for. 
+  
   mutate(age_death2 = if_else(age_death>=147, 200, age_death))%>%
   
+  #adding "event" column. 
+  mutate(event = case_when(age_death2 >150~0,
+                           age_death2 <=150~1))%>%
   
   
-  mutate(event = case_when(age_death2 >149~0,
-                           age_death2 <=149~1))%>%
-  arrange(genes_ko)->cohort_survival3
+  #sorting by genes_ko and saving object. 
+  arrange(genes_ko
+          
+          )->cohort_survival2
   
 
-cohort_survival3%>%
-  view()
 
 
 
 
-
-
-
-
-
-
-cohort_info%>%
-  arrange(genes_ko)%>%
-  view()
-
-
-
-
-
+#getting palette from legend. 
 
   #palette info
-cohort_info%>%
-  
+cohort_legend%>%
   arrange(genes_ko)%>%
   pull(hex
        
        )->cohort_palette
   
-#legend order
-cohort_info%>%
+#getting legend order from legend. 
+cohort_legend%>%
   arrange(genes_ko)%>%
   pull(cohort
        
@@ -147,22 +183,13 @@ cohort_info%>%
 
 
 
-leg_order
-cohort_palette
+#creating survfit object. 
+survfit(Surv(time = age_death2, event = event)~genes_ko, data = cohort_survival2)->cohort_surv
+
+
+
+#finally, plotting:
 #overall survival
-
-cohort_palette%>%
-  view()
-
-cohort_info%>%
-  view()
-
-cohort_survival2%>%
-  view()
-
-
-survfit(Surv(time = age_death2, event = event)~genes_ko, data = cohort_survival3)->cohort_surv
-
 
 cohort_surv%>%
   ggsurvplot(xlim = c(0, 150),
@@ -178,28 +205,69 @@ cohort_surv%>%
              xlab = "Time Post Injection (Days)",
              legend.title = "cohort",
              legend.lab = leg_order
-  )
+  )->plot
 
+#export plot - add export to code. 
+plot
+
+##########################################################################
+##########################################################################
+
+#statistics:
+sink("output.txt")
 cohort_surv%>%
   summary()
 
 
-
-
-
-#statistics:
-
 survdiff(Surv(time = age_death2, event = event) ~ genes_ko, 
-         data = cohort_survival3)%>%
-  as_tibble()
+         data = cohort_survival2)
+
+pairwise_survdiff(Surv(time = age_death2, event = event) ~ genes_ko, 
+         data = cohort_survival2, p.adjust.method = "BH")->pairwise
+
+pairwise_survdiff(Surv(time = age_death2, event = event) ~ genes_ko, 
+        data = cohort_survival2)
 
 
+
+sink(NULL)
 
 
 pairwise_survdiff(Surv(time = age_death2, event = event) ~ genes_ko, 
-         data = cohort_survival3, p.adjust.method = "BH")
+                  data = cohort_survival2, p.adjust.method = "BH")->pairwise
 
-pairwise_survdiff(Surv(time = age_death2, event = event) ~ genes_ko, 
-                  data = cohort_survival3)%>%
+
+class(pairwise)
+
+list(pairwise)%>%
+  as_tibble(
+    validate = NULL,
+    .rows = NULL,
+    .name_repair = c("check_unique", "unique", "universal", "minimal")
+  )
+
+list(pairwise)%>%
+  as_tibble(.name_repair= c("universal"))
+
+
+lapply(list(pairwise), list)
+
+
+read.delim("output.txt")%>%
+  as_tibble()%>%
   view()
 
+
+pairwise
+
+library(fmtr)
+library(libr)
+
+
+
+pairwise[["p.value"]]%>%
+  as_tibble()%>%
+  view()
+
+
+pairwise[["method"]]
