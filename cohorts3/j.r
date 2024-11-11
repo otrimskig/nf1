47c9d96cb7ce2d19cccdf3fa910b94794934f68e
Garrett Otrimski <u1413890@hci.utah.edu>
2024-11-11 14:25:05 -0700

    exporting most cleaned cohort data

---
+++ cohorts3/j.r
@@ -0,0 +1,15 @@
+xlsx::write.xlsx(c, "ds/cohorts-2024-01-18.xlsx", sheetName = "Sheet1", 
+           col.names = TRUE, row.names = TRUE, append = FALSE)
+
+
+
+# "C:\Users\u1413890\jre\jdk-23.0.1\lib"> 
+# 
+# # Set JAVA_HOME with correctly formatted path
+# Sys.setenv(JAVA_HOME = "C:/Users/u1413890/jre")
+# 
+# 
+# 
+# install.packages("rJava")
+# install.packages("xlsx")
+# install.packages("rJava", configure.args = "--with-java-home='C:/Users/u1413890/jre'")
