387e48c0692587dec807fa519b8a9a2ae9608f5d
Garrett Otrimski <u1413890@hci.utah.edu>
2024-11-12 10:30:36 -0700

    rearrange files

--- cohorts3/useful.R
+++
@@ -1,7 +0,0 @@
-mutate(
-  consistent_date = case_when(
-    str_detect(injection_date, "/") ~ mdy(injection_date),
-    str_detect(injection_date, "-") ~ ymd(injection_date),
-    TRUE ~ NA_Date_
-  )
-)
